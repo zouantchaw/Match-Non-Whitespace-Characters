2810e2fb28002b4c0f127ac7b821daff28d3fd43
Wielfried Zouantcha <44246692+zouantchaw@users.noreply.github.com>
2021-10-28 14:53:40 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+// Change the regex countNonWhiteSpace to look for multiple non-whitespace characters in a string.
+
+
+let sample = "Whitespace is important in separating words";
+let countNonWhiteSpace = /\S/g; 
+let result = sample.match(countNonWhiteSpace);
+console.log(result)
